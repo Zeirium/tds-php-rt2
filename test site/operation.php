2bdf0fcb3aa33d2f3675c1da56fd2a963f8625ca
Paul FAUCON <Paul FAUCON>
2017-03-02 18:29:18 +0100

    fin

---
+++ test site/operation.php
@@ -0,0 +1,15 @@
+<form>
+<input type="number"name="a">
+<input type="number"name="b">
+<input type="submit" value="Ajouter">
+</form>
+<?php
+
+
+if(isset($_GET["a"]) && isset($_GET["b"])) {
+$resultat=$_GET["a"]+$_GET["b"];
+echo "la somme est $resultat";
+
+     }
+
+?>
